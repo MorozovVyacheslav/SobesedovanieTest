43b174689921fd8d25cbbe689003c7c135db4ffa
Vyacheslav <verdark768@yandex.ru>
2017-10-02 13:13:04 +0300

    Prodakshen
    Fix oop

---
+++ ConsoleApp1/ConsoleApp1/Animal.cs
@@ -0,0 +1,133 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface irealise
+    {
+        string Realise();
+    }
+ 
+
+
+    class BasaMethods
+    {
+        public void Show(List<string> massivedinamic)
+        {
+            int i = 1;
+            foreach (var item in massivedinamic)
+            {
+
+                Console.WriteLine("{0} {1}", i, item);
+                i++;
+            }
+
+
+        }
+
+        public void Remove(List<string> massivedinamic, int x)
+        {
+
+            massivedinamic.RemoveAt(x - 1);
+
+
+
+        }
+        public virtual string Realise() 
+        {
+            return null;
+        }
+
+
+
+    }
+
+    class Person : irealise
+    {
+        string FirstName;
+        string LastName;
+        int AgeProduct;
+
+        public Person(string FirstName,
+        string LastName,
+        int AgeProduct)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.AgeProduct = AgeProduct;
+
+
+        }
+        public string Realise ()
+        {
+
+            return String.Format("[{0}] [{1}] {2} {3} {4}", DateTime.Now, GetType().Name, FirstName, LastName, AgeProduct);
+        }
+    }
+
+    class Car : irealise
+    {
+        string Marka; int AgeProduct;
+
+        public Car(string Marka, int AgeProduct)
+        {
+            this.Marka = Marka;
+            this.AgeProduct = AgeProduct;
+
+
+        }
+
+
+        public  string Realise()
+        {
+            return String.Format("[{0}] [{1}] {2} {3}", DateTime.Now, GetType().Name, Marka, AgeProduct);
+
+
+        }
+    }
+
+    class Message : irealise
+    {
+        string message;
+
+        public Message(string message)
+        {
+            this.message = message;
+        }
+
+
+
+
+        public string Realise()
+        {
+
+
+            return String.Format("[{0}] [{1}] {2} ", DateTime.Now, GetType().Name, message);
+
+
+        }
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+
+    delegate void bobr();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> spisok = new List<string>();
+            string chose = null;
+            string tire = new string('-', 30);
+            BasaMethods show = new BasaMethods();
+           
+         
+            do
+                {
+             
+
+
+
+
+
+             Console.WriteLine("\n1 .Print data \n2 .Add item \n3 .Remove item \nQ .Exit \n");
+
+            chose =  Console.ReadLine();
+
+             switch (chose)
+        {
+            case "1":
+                        Console.WriteLine("{0}", tire);
+                        show.Show(spisok);
+
+                        break;
+
+            case "2":
+                        Console.WriteLine("{0}", tire);
+                        string swatch;
+                        do
+                        {
+                            Console.WriteLine("\n1 .ADD Message \n2 .ADD Car \n3 .ADD person \nQ .Exit \n");
+                      swatch = Console.ReadLine();
+
+                            switch (swatch)
+                            {
+                                case "1":
+
+                                    Console.WriteLine("{0}\n Введите сообщение \n{1}", tire, tire);
+                                    
+                                    
+                                    spisok.Add(new Message(Console.ReadLine()).Realise());
+                                    Console.WriteLine("{0}\n Сообщение добавлено \n{1}", tire, tire);
+
+                                    break;
+
+
+                                case "2":
+                                    try
+                                    {
+                                        Console.WriteLine("{0}\n Введите Марку машины \n{1}", tire, tire);
+                                        string firstname1 = Console.ReadLine();
+                                        Console.WriteLine("{0}\n Дату производства \n{1}", tire, tire);
+                                        int age1 = int.Parse(Console.ReadLine());
+
+
+                                         spisok.Add(new Car(firstname1, age1).Realise())
+                                        ;
+
+                                        Console.WriteLine("{0}\n Марка , дата производства добавлены\n{1}", tire, tire);
+                                   
+
+                                    }
+                                    catch (FormatException)
+                                    {
+                                        Console.WriteLine("Неправильный тип данных!!!!");
+                                        Console.WriteLine("{0}", tire);
+                                    }
+
+                                    break;
+
+
+
+
+
+
+                                case "3":
+
+                                    Console.WriteLine("{0}\n Введите First Name \n{1}", tire, tire);
+                                    string firstname = Console.ReadLine();
+                                    Console.WriteLine("{0}\n Введите Last Name \n{1}", tire, tire);
+                                    string LaseName = Console.ReadLine();
+                                    Console.WriteLine("{0}\n Введите возраст \n{1}", tire, tire);
+                                    try
+                                    {
+                                        int age = int.Parse(Console.ReadLine());
+                                        //spisok.Add(person.Realise(firstname, LaseName, age));
+                                       ;
+                                        spisok.Add(new Person(firstname, LaseName, age).Realise());
+
+
+
+                                        Console.WriteLine("{0}\n Имя , Фамилия , Возраст добавлены\n{1}", tire, tire);
+
+                                    }
+                                    catch (FormatException)
+                                    {
+
+                                        Console.WriteLine("Неправильный тип данных!!!!");
+                                        Console.WriteLine("{0}", tire);
+                                    }
+                                    
+
+                                   
+
+                                    break;
+
+
+
+                                default:
+
+                                    Console.WriteLine("{0}\n Введите корректное значение \n{1}", tire, tire);
+                                    break;
+                            }
+                        } while (swatch != "Q");
+
+
+
+                        break;
+
+
+
+
+                    case "3":
+                        Console.WriteLine("{0}", tire);
+                        Console.WriteLine("Для удаления записи выберите индекс");
+                        show.Show(spisok);
+                        show.Remove(spisok, int.Parse(Console.ReadLine()));
+                        Console.WriteLine("Запись удалена");
+                        Console.WriteLine("{0}", tire);
+
+
+                        break;
+
+
+
+                 default:
+                     
+                     Console.WriteLine("{0}\n Введите корректное значение \n{1}", tire , tire); 
+                 break;
+
+             }
+
+
+                 
+
+
+    } while (chose !="Q");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
+
+
+
+    
+
+
+
+
+    
+    
+
